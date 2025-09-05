a16bbd979e0b47001b6503dd4d2dde922bbe409d
Ogunnaike Michael <ogunnaikeokikiola@gmail.com>
2025-09-05 19:52:16 +0100

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# --- Page Config ---
+st.set_page_config(page_title="Happy Birthday Joy 🎉", layout="centered")
+
+# --- Title and Balloons ---
+st.title("🎉 Happy Birthday Beautiful! 🎂")
+st.balloons()
+
+# --- Personalized Message ---
+st.markdown("I don’t know if you realize it, but you make life a lot brighter just by being yourself." 
+            "You’ve got this smile that could outshine candles on any cake, and a vibe that’s honestly addictive (in the best way 😉)")
+st.markdown("I hope today gives you every reason to laugh, feel loved, and be spoiled — because you deserve nothing less." 
+            "Someone as amazing as you should have a day that feels just as special.")
+st.markdown("So here’s me, wishing you endless happiness, surprises that make you grin, and maybe a little reminder that you’re unforgettable 👀.")
+st.markdown("Happy Birthday once again, trouble — I hope this year brings you everything your heart secretly wishes for 💕")
